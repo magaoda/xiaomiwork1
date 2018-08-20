8f4a3ceaacc154aa08ac7e862791034360cd5427
magaoda <952193774@qq.com>
2018-08-20 19:00:46 +0800

    mgd

--- js/index.js
+++ js/index.js
@@ -13,12 +13,17 @@ window.onload=function() {
         car.style.height = "99px";
         car.style.boxShadow = "0 7px 6px rgba(0,0,0,0.2)";
         car.style.lineHeight = "99px";
+        // shop.style.background="#fff";
+        // shop.style.color="#ff6700";
     }
     shop.onmouseleave = function () {
         car.style.height = 0;
         car.style.boxShadow = "none";
         car.style.lineHeight = "none";
+        // shop.style.background="#fff";
+        // shop.style.color="#ff6700";
     }
+    
 
     let sou=document.querySelector(".sou");
     let son11=document.querySelector(".son11");
@@ -41,24 +46,52 @@ window.onload=function() {
         ss.style.display="none";
     }
 
+    // let bigBox = document.getElementsByClassName("bigBox")[0];
+    // let logoBox = bigBox.getElementsByClassName("logoBox")[0];
+    // let nav1 = logoBox.getElementsByClassName("nav1")[0];
+    // let xia = nav1.getElementsByClassName("xia");
+    // let logoCon = document.getElementsByClassName("logoCon");
+    // console.log(xia, logoCon);
+    // for (let i = 0; i < xia.length - 2; i++) {
+    //     xia[i].onmouseenter = function () {
+    //         for (let j = 0; j < xia.length - 2; j++) {
+    //             // logoCon[j].style.display = "none";
+    //         }
+    //         // logoCon[i].style.display = "block";
+    //         logoCon[i].style.height = "230px";
+    //         logoCon[i].style.background = "#fff";
+    //         logoCon[i].style.borderTop = "1px solid #E0E0E0";
+    //         logoCon[i].style.boxShadow = "0 7px 6px rgba(0,0,0,0.2)";
+    //     }
+    //     xia[i].onmouseleave = function () {
+    //         // logoCon[i].style.display = "none";
+    //         logoCon[i].style.height = 0;
+    //         logoCon[i].style.background = "none";
+    //         logoCon[i].style.boxShadow = "none";
+    //         logoCon[i].style.borderTop = "none";
+    //     }
+    // }
+
     let bigBox = document.getElementsByClassName("bigBox")[0];
     let logoBox = bigBox.getElementsByClassName("logoBox")[0];
     let nav1 = logoBox.getElementsByClassName("nav1")[0];
-    let xia = nav1.getElementsByClassName("xia");
+    // let xia = nav1.getElementsByClassName("xia");
+    let itemBox = document.querySelectorAll(".itemBox");
     let logoCon = document.getElementsByClassName("logoCon");
-    console.log(xia, logoCon);
-    for (let i = 0; i < xia.length - 2; i++) {
-        xia[i].onmouseenter = function () {
-            for (let j = 0; j < xia.length - 2; j++) {
-                // logoCon[j].style.display = "none";
+    console.log(itemBox, logoCon);
+    for (let i = 0; i < itemBox.length; i++) {
+        itemBox[i].onmouseenter = function () {
+            for (let j = 0; j < 8; j++) {
+                logoCon[j].style.display = "none";
+                logoCon[j].style.height = 0;
             }
-            // logoCon[i].style.display = "block";
+            logoCon[i].style.display = "block";
             logoCon[i].style.height = "230px";
             logoCon[i].style.background = "#fff";
             logoCon[i].style.borderTop = "1px solid #E0E0E0";
             logoCon[i].style.boxShadow = "0 7px 6px rgba(0,0,0,0.2)";
         }
-        xia[i].onmouseleave = function () {
+        itemBox[i].onmouseleave = function () {
             // logoCon[i].style.display = "none";
             logoCon[i].style.height = 0;
             logoCon[i].style.background = "none";
@@ -111,8 +144,8 @@ window.onload=function() {
                 navCon[i].style.color = "red";
                 navCon[i].style.border = "2px soild red";
             }
-
         }
+
     }
 
     let goods = document.getElementsByClassName("goods")[0];
@@ -126,70 +159,70 @@ window.onload=function() {
     let zhoubian = document.getElementsByClassName("zhoubian")[0];
     xm(zhoubian);
 
-    let bigCon = document.getElementsByClassName("bigCon")[0];
-    let wraper = bigCon.getElementsByClassName("wraper")[0];
-    let zhuan = wraper.getElementsByClassName("zhuan")[0];
-    let a = zhuan.getElementsByTagName("a");
-    let left = bigCon.getElementsByClassName("jiantou1")[0];
-    let right = bigCon.getElementsByClassName("jiantou2")[0];
-    let btns = bigCon.getElementsByClassName("btns")[0];
-    let li = btns.getElementsByTagName("li");
-    console.log(li);
-    console.log(a);
+    // let bigCon = document.getElementsByClassName("bigCon")[0];
+    // let wraper = bigCon.getElementsByClassName("wraper")[0];
+    // let zhuan = wraper.getElementsByClassName("zhuan")[0];
+    // let a = zhuan.getElementsByTagName("a");
+    // let left = bigCon.getElementsByClassName("jiantou1")[0];
+    // let right = bigCon.getElementsByClassName("jiantou2")[0];
+    // let btns = bigCon.getElementsByClassName("btns")[0];
+    // let li = btns.getElementsByTagName("li");
+    // console.log(li);
+    // console.log(a);
 
-    let t = setInterval(move, 1000);
-    let num = 0;
+    // let t = setInterval(move, 2000);
+    // let num = 0;
 
-    function move() {
-        num++;
-        if (num == a.length) {
-            num = 0;
-        }
-        for (let i = 0; i < a.length; i++) {
-            a[i].style.zIndex = 5;
-            li[i].className = "";
-        }
-        a[num].style.zIndex = 10;
-        li[num].className = " hot";
-    }
+    // function move() {
+    //     num++;
+    //     if (num == a.length) {
+    //         num = 0;
+    //     }
+    //     for (let i = 0; i < a.length; i++) {
+    //         a[i].style.zIndex = 5;
+    //         li[i].className = "";
+    //     }
+    //     a[num].style.zIndex = 10;
+    //     li[num].className = " hot";
+    // }
 
-    bigCon.onmouseenter = function () {
-        clearInterval(t);
-    }
-    bigCon.onmouseleave = function () {
-        t = setInterval(move, 1000);
-    }
-    right.onclick = function () {
-        move1();
-    }
+    // bigCon.onmouseenter = function () {
+    //     clearInterval(t);
+    // }
+    // bigCon.onmouseleave = function () {
+    //     t = setInterval(move, 1000);
+    // }
+    // right.onclick = function () {
+    //     move1();
+    // }
 
-    function move1() {
-        num--;
-        if (num < 0) {
-            num = a.length - 1;
-        }
-        for (let j = 0; j < a.length; j++) {
-            a[j].style.zIndex = 5;
-            li[j].className = "";
-        }
-        a[num].style.zIndex = 10;
-        li[num].className = "hot";
-    }
+    // function move1() {
+    //     num--;
+    //     if (num < 0) {
+    //         num = a.length - 1;
+    //     }
+    //     for (let j = 0; j < a.length; j++) {
+    //         a[j].style.zIndex = 5;
+    //         li[j].className = "";
+    //     }
+    //     a[num].style.zIndex = 10;
+    //     li[num].className = "hot";
+    // }
 
-    left.onclick = function () {
-        move();
-    }
-    for (let x = 0; x < li.length; x++) {
-        li[x].onclick = function () {
-            for (let y = 0; y < li.length; y++) {
-                a[y].style.zIndex = 5;
-                li[y].className = "";
-            }
-            a[x].style.zIndex = 10;
-            li[x].className = "hot";
-            num = x;
-        }
-    }
+    // left.onclick = function () {
+    //     move();
+    // }
+    // for (let x = 0; x < li.length; x++) {
+    //     li[x].onclick = function () {
+    //         for (let y = 0; y < li.length; y++) {
+    //             a[y].style.zIndex = 5;
+    //             li[y].className = "";
+    //         }
+    //         a[x].style.zIndex = 10;
+    //         li[x].className = "hot";
+    //         num = x;
+    //     }
+    // }
     // let neirong=document.querySelector(".neirong .con .box1 .list");
     // let list=document.querySelectorAll(".list");
     // let btns1=document.querySelectorAll(".btns1");
@@ -344,105 +377,110 @@ window.onload=function() {
     bo.forEach(function (element) {
         lbt(element);
     })
-    //
-    // let bigCon=document.querySelector(" .bigCon ");
-    // let lun=document.querySelectorAll(".lun");
-    // let left=bigCon.getElementsByClassName("jiantou1")[0];
-    // let right=bigCon.getElementsByClassName("jiantou2")[0];
-    // let btns=bigCon.getElementsByClassName("btns")[0];
-    // let li=btns.getElementsByTagName("li");
-    // console.log(bigCon,lun,li);
-    // let now=0;
-    // let next=0;
-    // let flag=true;
-    // let widths=parseInt(getComputedStyle(bigCon,null).width);
-    // console.log(widths);
-    // let t=setInterval(move,2000);
-    // function move() {
-    //     next++;
-    //     if (next==lun.length){
-    //         next=0;
-    //     }
-    //     // for(let i=0;i<li.length;i++){
-    //     //      li[i].className="";
-    //     // }
-    //     //  li[now].className="hot";
-    //     lun[next].style.left=-widths+"px";
-    //     animate(lun[now],{left:widths},function () {
-    //         flag=true;
-    //     });
-    //     animate(lun[next],{left:0});
-    //     li[next].classList.add("hot");
-    //     li[now].classList.remove("hot");
-    //     now=next;
-    // }
-    // bigCon.onmouseenter=function () {
-    //     clearInterval(t);
-    // }
-    // bigCon.onmouseleave=function () {
-    //     t=setInterval(move,1000);
-    // }
-    // left.onclick=function () {
-    //     if(flag==false){
-    //         return;
-    //     }
-    //     if(next==lun.length-1){
-    //         return;
-    //     }
-    //     flag=false;
-    //     move();
-    // }
-    // function move1() {
-    //     next--;
-    //     if(next<0){
-    //         next=lun.length-1;
-    //     }
-    //     // for(let x=0;x<li.length;x++){
-    //     //     li[x].className="";
-    //     // }
-    //     // li[now].className="hot";
-    //     lun[next].style.left=widths+"px";
-    //     animate(lun[now],{left:-widths},function () {
-    //         flag=true;
-    //     });
-    //     animate(lun[next],{left:0});
-    //     li[next].classList.add("hot");
-    //     li[now].classList.remove("hot");
-    //     now=next;
-    // }
-    // right.onclick=function () {
-    //     if(flag==false){
-    //         return;
-    //     }
-    //     if(next==0){
-    //         return;
-    //     }
-    //     flag=false;
-    //     move1();
-    // }
-    // for(let i=0;i<li.length;i++){
-    //     li[i].onclick=function () {
-    //         if (i > now) {
-    //             lun[i].style.left=-widths+"px";
-    //             animate(lun[now], {left: -widths});
-    //             animate(lun[i], {left: 0});
-    //             li[i].classList.add("hot");
-    //             li[now].classList.remove("hot");
-    //         }
-    //         if (i == now) {
-    //             return;
-    //         }
-    //         if (i < now) {
-    //             lun[i].style.left=widths+"px";
-    //             animate(lun[now], {left: widths});
-    //             animate(lun[i], {left: 0});
-    //             li[i].classList.add("hot");
-    //             li[now].classList.remove("hot");
-    //         }
-    //         now=next=i;
-    //     }
-    //
-    // }
+    
+    let bigCon=document.querySelector(" .bigCon ");
+    let lun=document.querySelectorAll(".lun");
+    let left=bigCon.getElementsByClassName("jiantou1")[0];
+    let right=bigCon.getElementsByClassName("jiantou2")[0];
+    let btns=bigCon.getElementsByClassName("btns")[0];
+    let li=btns.getElementsByTagName("li");
+    console.log(bigCon,lun,li);
+    let now=0;
+    let next=0;
+    let flag=true;
+    let widths=parseInt(getComputedStyle(bigCon,null).width);
+    console.log(widths);
+    let t=setInterval(move,2000);
+    lun.forEach(function(value,index){
+        if(index!=0){
+            value.style.left=widths+"px";
+        }
+    })
+    function move() {
+        next++;
+        if (next==lun.length){
+            next=0;
+        }
+        // for(let i=0;i<li.length;i++){
+        //      li[i].className="";
+        // }
+        //  li[now].className="hot";
+        lun[next].style.left=widths+"px";
+        animate(lun[now],{left:-widths},function () {
+            flag=true;
+        });
+        animate(lun[next],{left:0});
+        li[next].classList.add("hot");
+        li[now].classList.remove("hot");
+        now=next;
+    }
+    bigCon.onmouseenter=function () {
+        clearInterval(t);
+    }
+    bigCon.onmouseleave=function () {
+        t=setInterval(move,1000);
+    }
+    left.onclick=function () {
+        if(flag==false){
+            return;
+        }
+        if(next==lun.length-1){
+            return;
+        }
+        flag=false;
+        move();
+    }
+    function move1() {
+        next--;
+        if(next<0){
+            next=lun.length-1;
+        }
+        // for(let x=0;x<li.length;x++){
+        //     li[x].className="";
+        // }
+        // li[now].className="hot";
+        lun[next].style.left=widths+"px";
+        animate(lun[now],{left:-widths},function () {
+            flag=true;
+        });
+        animate(lun[next],{left:0});
+        li[next].classList.add("hot");
+        li[now].classList.remove("hot");
+        now=next;
+    }
+    right.onclick=function () {
+        if(flag==false){
+            return;
+        }
+        if(next==0){
+            return;
+        }
+        flag=false;
+        move1();
+    }
+    for(let i=0;i<li.length;i++){
+        li[i].onclick=function () {
+            if (i > now) {
+                lun[i].style.left=widths+"px";
+                animate(lun[now], {left: widths});
+                animate(lun[i], {left: 0});
+                li[i].classList.add("hot");
+                li[now].classList.remove("hot");
+            }
+            if (i == now) {
+                return;
+            }
+            if (i < now) {
+                lun[i].style.left=widths+"px";
+                animate(lun[now], {left: widths});
+                animate(lun[i], {left: 0});
+                li[i].classList.add("hot");
+                li[now].classList.remove("hot");
+            }
+            now=next=i;
+        }
+    
+    }
     let box2=document.querySelector(".box2")
     let son2=box2.querySelectorAll(".left .box .box2 .son2");
     console.log(son2);
@@ -459,6 +497,7 @@ window.onload=function() {
         let arr=[];
         let now = new Date();
         let future = new Date(2018,7,10,18);
+       
         let sj = Math.floor(future.getTime() - now.getTime())/1000;
         // let month = Math.floor(sj / (30 * 24 * 60 * 60));
         // sj = sj % (30 * 24 * 60 * 60);
@@ -468,6 +507,10 @@ window.onload=function() {
         // sj = sj % (24 * 60 * 60);
         // arr.push(day);
        
+        if(now>future){
+            future=future+(24 * 60 * 60)
+        }
+
         let hour = Math.floor(sj / (60 * 60));
         sj = sj % (60 * 60);
         if(hour>=0 && hour<=9){
